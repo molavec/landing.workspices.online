401cfc25d8bac19951d424edd4dcd50a762d5fb1
Miguel Olave <molavec@gmail.com>
2025-10-26 18:54:20 -0300

    adapt style

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -9,10 +9,10 @@ useSeoMeta({
 
 <template>
   <!-- <ContentRenderer v-if="home" :value="home" /> -->
-  <div class="flex flex-col gap-4 items-center justify-center h-lvh w-full">
-    <div class="text-4xl">Workspices</div>
-    <img src="/workspices.png" alt="workspices image" class="w-md" />
-    <a href="https://beta.workspices.online" class="">Go to App</a>
+  <div class="flex flex-col gap-4 items-center justify-center h-lvh w-full bg-[#fbf7f1] text-[#3e443e]">
+    <!-- <div class="text-4xl">Workspices</div> -->
+    <img src="/workspices.png" alt="workspices image" class="max-w-xs" />
+    <a href="https://app.workspices.online" class="py-2 px-4 bg-[#82c435] text-white font-semibold">Start Now</a>
   </div>
 
 </template>
