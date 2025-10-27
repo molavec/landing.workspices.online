5c3783b64954484489aa0f3567e8fd5952754ab1
Miguel Olave <molavec@gmail.com>
2025-10-27 02:03:08 -0300

    cloudflare_pages as preset

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,7 +6,7 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
 
   nitro: {
-    preset: "cloudflare_module",
+    preset: "cloudflare_pages",
     cloudflare: {
       deployConfig: true,
       nodeCompat:true
