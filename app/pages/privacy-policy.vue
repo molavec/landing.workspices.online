8fa601e56b62af2feddcdb118353d7a0242a0905
Miguel Olave <molavec@gmail.com>
2025-10-27 02:00:13 -0300

    terms as page

---
+++ app/pages/privacy-policy.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    privacy-policy
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style>
+
+</style>
---
+++ app/pages/privacy.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    privacy
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style>
+
+</style>
